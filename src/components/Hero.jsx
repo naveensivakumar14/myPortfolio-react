bae2c8f7141f94c8ad90360ce0b97fa6019c882a
naveensivakumar14 <ersnaveenkumar@gmail.com>
2024-09-28 12:30:54 +0530

    Resume download button  added to Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
+
+import Resume from "../assets/images/Naveen S.pdf"
+
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { useEffect } from 'react'
@@ -24,10 +27,9 @@ function Hero(){
             <div className="w-[80%] md:w-1/2 ">
                 <p className="text-2xl p-2 font-bold">Hi, I am</p>
                 <h1 className="text-4xl p-2 font-bold md:text-6xl">Naveenkumar</h1>
-                
-
                 <h3 className="text-xl p-2 font-bold md:text-2xl ">MERN Stack Developer</h3>
-                
+                {/* <button className="bg-[#16423C] text-white border-none rounded-md px-2 py-1"></button> */}
+                <button type="button" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-2 me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"><a href={Resume} download='Naveenkumar'>Resume</a></button>
             </div>
 
 
