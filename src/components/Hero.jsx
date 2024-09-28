5e5f4926afcb306eb475246d616795195146d27a
Naveen_Sivakumar <45707585+naveensivakumar14@users.noreply.github.com>
2024-09-28 12:50:00 +0530

    Update Hero.jsx resume button my-2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ function Hero(){
                 <p className="text-2xl p-2 font-bold">Hi, I am</p>
                 <h1 className="text-4xl p-2 font-bold md:text-6xl">Naveenkumar</h1>
                 <h3 className="text-xl p-2 font-bold md:text-2xl ">MERN Stack Developer</h3>
-                <button type="button" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-2 me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"><a href={Resume} download='Naveenkumar'>Resume</a></button>
+                <button type="button" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-2 me-2 my-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"><a href={Resume} download='Naveenkumar'>Resume</a></button>
 
                 
 
